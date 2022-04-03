2e1bf7accea861cb11837072aef8a9a45068cf14
icedvigilante <wm.ryan.mooney@gmail.com>
2022-04-03 12:23:47 -0800

    Complete Pixel Canvas Layout

--- pixl/pixl.go
+++ pixl/pixl.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"github.com/simpleittools/pixl/apptype"
+	"github.com/simpleittools/pixl/pxcanvas"
 	"github.com/simpleittools/pixl/swatch"
 	"github.com/simpleittools/pixl/ui"
 	"image/color"
@@ -16,7 +18,18 @@ func main() {
 		SwatchSelected: 0,
 	}
 
+	pixlCanvasConfig := apptype.PxCanvasConfig{
+		DrawingArea:  fyne.NewSize(600, 600),
+		CanvasOffset: fyne.NewPos(0, 0),
+		PxRows:       10,
+		PxCols:       10,
+		PxSize:       30,
+	}
+
+	pixlCanvas := pxcanvas.NewPxCanvas(&state, pixlCanvasConfig)
+
 	appInit := ui.AppInit{
+		PixlCanvas: pixlCanvas,
 		PixlWindow: pixlWindow,
 		State:      &state,
 		Swatches:   make([]*swatch.Swatch, 0, 64),
--- ui/types.go
+++ ui/types.go
@@ -3,10 +3,12 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"github.com/simpleittools/pixl/apptype"
+	"github.com/simpleittools/pixl/pxcanvas"
 	"github.com/simpleittools/pixl/swatch"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch
